d3419760b7052d9116469851cab7ceceef39555a
dvvedz <oskar9899@gmail.com>
2021-03-02 17:17:43 +0100

    v1.1

--- hoststojson.py
+++ hoststojson.py
@@ -2,11 +2,13 @@
 
 import sys
 import os
+import re
 
 def generate_2d_list():
     count = 0
+    input_file = sys.argv[1]
 
-    with open(sys.argv[1], "r") as f:
+    with open(input_file, "r") as f:
         list_of_2d = []
 
         for line in f:
@@ -16,33 +18,34 @@ def generate_2d_list():
 
             if not "#" in line:
                 word = line.split(" ")
-
                 for w in word:
-                    if w:
+                    if w and len(word) > 1:
                         list_2d.append(w)
-                        list_of_2d.append(list_2d)
-                        break
-                    break
+                if line:
+                    list_of_2d.append(list_2d)
+
 
     return list_of_2d
-print(generate_2d_list())
-
-for g in generate_2d_list():
-    print(g)
-
 
 
 def testing():
     print('{\n    "hosts": {')
-    for list in generate_2d_list():
-        ip = list[0]
-        hosts = list[1:]
-        hosts_with_comma = hosts[:-1]
-        host_last = hosts[-1]
+    for listi in generate_2d_list():
+        try:
+            ip = listi[0]
+            hosts = listi[1:]
+            hosts_with_comma = hosts[:-1]
+            host_last = hosts[-1]
 
-        print('        "', end="")
-        for lis in hosts_with_comma:
-            print(f'{lis} ', end="")
-        print(f'{host_last}": "{ip}",\n', end="")
-    print('        "DELETE":"ME"')
+            print('        "', end="")
+            for lis in hosts_with_comma:
+                print(f'{lis} ', end="")
+
+            print(f'{host_last}": "{ip}",\n', end="")
+        except IndexError:
+            print(f'        "ERROR bellow this:": "{host_last} {ip}",')
+
+    print('        "blah": "Delete ME"')
     print("    }\n}")
+    
+testing()
